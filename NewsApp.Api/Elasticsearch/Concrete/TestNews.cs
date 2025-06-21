dd06b1959ddc2b9f77b197d11628bc1914811ca6
Haçik Ulu <hacikd2@gmail.com>
2025-06-21 17:02:37 +0300

    Elasticsearch configuration started and test data edited.

---
+++ NewsApp.Api/Elasticsearch/Concrete/TestNews.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Nest;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NewsApp.Api.Workers
+{
+    public class TestNews : BackgroundService
+    {
+        private readonly ILogger<TestNews> _logger;
+        private readonly IElasticClient _client;
+
+        public TestNews(ILogger<TestNews> logger, IElasticClient client)
+        {
+            _logger = logger;
+            _client = client;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var exampleNewsData = new
+            {
+                Title = "Test Title",
+                Content = "This is a sample news content for elasticsearch"
+            };
+
+            var response = await _client.IndexDocumentAsync(exampleNewsData, stoppingToken);
+
+            if (response.IsValid)
+            {
+                _logger.LogInformation(response.Id);
+            }
+        }
+    }
+}
--- NewsApp.Api/Program.cs
+++ NewsApp.Api/Program.cs
@@ -1,14 +1,34 @@
+using Elasticsearch.Net;
+using Nest;
+using NewsApp.Api.Models;
 using NewsApp.Api.Services.Abstract;
 using NewsApp.Api.Services.Concrete;
+using NewsApp.Api.Workers;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+//builder.Configuration.AddJsonFile("secrets.json", optional: false, reloadOnChange: true);
+builder.Configuration.AddUserSecrets<Program>();
+var config = builder.Configuration;
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<INewsAppService, NewsAppService>();
+
+builder.Services.AddSingleton<IElasticClient>(x =>
+{
+    var settings = new ConnectionSettings(config["cloudId"], new BasicAuthenticationCredentials(
+        "elastic", config["password"]))
+        .DefaultIndex("news")
+        .DefaultMappingFor<NewsAppDto>(i => i.IndexName("news-app-demo"));
+
+    return new ElasticClient(settings);
+});
+
+builder.Services.AddHostedService<TestNews>();
 
 var app = builder.Build();
 
@@ -19,7 +39,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
+
+
+#region VideoSrc
+// https://www.youtube.com/watch?v=tw9svKWq6tg&ab_channel=dotnetFlix
+// https://cloud.elastic.co/deployments/b5efd44d51b041638fcb92869db2c65f
+#endregion
