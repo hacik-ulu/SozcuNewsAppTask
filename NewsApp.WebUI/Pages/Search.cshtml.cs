b811e3fed3e29c2c80fb88e599990932185677fd
Haçik Ulu <hacikd2@gmail.com>
2025-06-23 18:35:24 +0300

    Searchbox editing.

--- NewsApp.WebUI/Pages/Search.cshtml.cs
+++ NewsApp.WebUI/Pages/Search.cshtml.cs
@@ -18,32 +18,54 @@ namespace NewsApp.WebUI.Pages
 
         public async Task OnGetAsync()
         {
-            Search = Request.Query["search"]; // tarayıcıdan gelen URL'deki ?search="" parametresini alıp Searche atıyoruz.
+            Search = Request.Query["search"]; // URL'den gelen arama parametresini al
 
-            if (string.IsNullOrEmpty(Search))
+            if (string.IsNullOrWhiteSpace(Search))
             {
-                return; //metod sonlanır, sayfa tekrar yuklenir;
+                return; // boşsa işlem yapma
             }
 
-            // Elasticsearche arama isteği gönderiyoruz SearchAsync() ile, arama kritlerlerini geçiyoruz, birden fazla arama kriteri olduğundan multimatch diyoruz.
-            var searchQuery = await _elasticClient.SearchAsync<NewsAppDto>(x => x
-                .Query(y => y
-                    .MultiMatch(z => z
-                        .Fields(t => t
-                            .Field(k => k.Title)
-                            .Field(w => w.Summary)
-                            .Field(p => p.Content)
-                            .Field(a => a.Author)
-                            .Field(c => c.Category)
+            //multimatch --> bir arama sorgusunun birden fazla fieldda kontrol edilmesini saglıyor.
+
+
+            var searchQuery = await _elasticClient.SearchAsync<NewsAppDto>(s => s
+                .Query(q => q
+                    .Bool(b => b // birden fazla sorguyu bir araya getirmek adına kullanıyoruz.
+                        .Should(
+                            // Fuzzy destekli MultiMatch araması
+                            m => m.MultiMatch(mm => mm  // Örneğin ilkay yazdığımızda tüm fieldlar için (multimatch) kontrol sağlıyoruz.
+                                .Fields(f => f
+                                    .Field(p => p.Title)
+                                    .Field(p => p.Summary)
+                                    .Field(p => p.Content)
+                                    .Field(p => p.Author)
+                                    .Field(p => p.Category)
+                                )
+                                .Query(Search)
+                                .Fuzziness(Fuzziness.Auto) // yazım yanlısları da olsa esneklik sağlıyoruz
+                                .PrefixLength(1) // benzer harf durumu
+                                .MaxExpansions(30) // kaç farklı varyasyon deniyoruz
+                            ),
+                            // Kelimenin yanlıs yazımı dısında kelimenin en basından eslestirilmesi islemi adına orneğin ilk yazıp ilkay sonuclar gelmesi adına TextQueryType kullanıyoruz.
+                            m => m.MultiMatch(mm => mm
+                                .Fields(f => f
+                                    .Field(p => p.Title)
+                                    .Field(p => p.Summary)
+                                    .Field(p => p.Content)
+                                    .Field(p => p.Author)
+                                    .Field(p => p.Category)
+                                )
+                                .Query(Search)
+                                .Type(TextQueryType.PhrasePrefix)
+                            )
                         )
-                        .Query(Search) // Kullanıcının girmis oldugu Search değerini bu alanlarda arıyoruz.
                     )
                 )
             );
 
             if (searchQuery.IsValid)
             {
-                NewsList = searchQuery.Documents.ToList(); 
+                NewsList = searchQuery.Documents.ToList();
             }
         }
     }
@@ -56,4 +78,12 @@ namespace NewsApp.WebUI.Pages
 //https://www.elastic.co/docs/reference/elasticsearch/clients/dotnet/query
 
 // https://stackoverflow.com/questions/31086987/searching-for-an-input-keyword-in-all-fields-of-an-elasticsearch-document-using
+
+//https://www.elastic.co/docs/reference/query-languages/query-dsl/query-dsl-fuzzy-query  --> fuzzy query ve max expansions
+
+//https://www.pipiho.com/es/7.7/en/query-dsl-multi-match-query.html
+
+//https://stackoverflow.com/questions/74867381/elasticsearch-partial-search-with-fuzziness-on-multiple-fields   --> Multimatch ile iki ayrı clause (yazım yanlısı ve kelime eslesmesi durumu)
+
+
 #endregion
