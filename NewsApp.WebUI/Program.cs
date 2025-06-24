e0a9d148f660cb3fe5d90562a7936b6dce0e4188
Haçik Ulu <hacikd2@gmail.com>
2025-06-24 21:50:16 +0300

    Project finished.

--- NewsApp.WebUI/Program.cs
+++ NewsApp.WebUI/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddSingleton<ElasticsearchContext>();
 
 // IElasticClient ile NEST kütüphanesini kullanabiliyor oluyoruz.
+// IElasticClient istediðimizde uygulama bize ElasticsearchContext.Client verecek.
 builder.Services.AddSingleton<IElasticClient>(x =>
 {
     var context = x.GetRequiredService<ElasticsearchContext>();
